833fc3cc2adf73f37d748622c471bd3b3ebaaade
ntyazhelkov <pronger95@gmail.com>
2021-04-27 19:55:35 +0300

    fix

--- src/main/java/ru/appline/controller/Controller.java
+++ src/main/java/ru/appline/controller/Controller.java
@@ -5,6 +5,7 @@ import ru.appline.logic.Pet;
 import ru.appline.logic.PetModel;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -35,15 +36,41 @@ public class Controller {
     @DeleteMapping(value = "/delPet", consumes = "application/json", produces = "application/json")
     public Map<String, String> deletePets(@RequestBody Map<String, Integer> id) {
         Map<String, String> message = new HashMap<String, String>();
-        petModel.getAll().remove(id.get("id"));
-        message.put("Message", "Питомец успешно удалён");
+        int i = 0;
+        for(Iterator<Integer> iterator = petModel.getAll().keySet().iterator(); iterator.hasNext(); ) {
+            Integer key = iterator.next();
+            if(key == id.get("id")) {
+                iterator.remove();
+                i++;
+            }
+            message.put("Message", "Питомец успешно удалён");
+        }
+        if (i == 0) {
+            message.put("Message", "Такого питомца нет!");
+        }
         return message;
     }
 
     @PutMapping(value = "/putPet", consumes = "application/json", produces = "application/json")
-    public Map<Integer, Pet> putPet(@RequestBody Map<String, String> pet) {
+    public Map<String, String> putPet(@RequestBody Map<String, String> pet) {
         Pet mainPet = new Pet(pet.get("name"), pet.get("type"), Integer.parseInt(pet.get("age")));
-        petModel.getAll().put(Integer.parseInt(pet.get("id")), mainPet);
-        return petModel.getAll();
+        Map<String, String> message = new HashMap<String, String>();
+        if (Integer.parseInt(pet.get("id")) > 0) {
+            int i = 0;
+            for(Iterator<Integer> iterator = petModel.getAll().keySet().iterator(); iterator.hasNext(); ) {
+                Integer key = iterator.next();
+                if(key == Integer.parseInt(pet.get("id"))) {
+                    petModel.getAll().put(Integer.parseInt(pet.get("id")), mainPet);
+                    i++;
+                }
+                message.put("Message", "Питомец успешно изменен");
+            }
+            if (i == 0) {
+                message.put("Message", "Такого питомца нет");
+            }
+        } else {
+            message.put("Message", "ID должен быть больше нуля!");
+        }
+        return message;
     }
 }
